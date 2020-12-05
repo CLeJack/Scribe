2b70982eb168755a10564c93ee4d8534bea2879f
CLeJack <cjacksonv3.0@gmail.com>
2020-12-05 12:12:42 -0600

    gui file cleanup

---
+++ Source/GUITabs.h
@@ -0,0 +1,26 @@
+/*
+  ==============================================================================
+
+    GUITabs.h
+    Created: 4 Dec 2020 11:44:58am
+    Author:  cjack
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+#include "GUIGlobals.h"
+#include "Stats.h"
+#include "CircularBuffer.h"
+
+class GuiTabs : public juce::TabbedComponent
+{
+public:
+
+    GuiTabs() : juce::TabbedComponent (juce::TabbedButtonBar::TabsAtTop){};
+    void currentTabChanged(int newCurrentTabIndex, const juce::String& newCurrentTabName) override;
+    GUIState guiState = GUIState::main;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GuiTabs);
+};
